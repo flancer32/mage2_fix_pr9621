e61cf58efd8cf9d0b7aaa4c00e072d3943e6ec47
Alex Gusev <alex@flancer64.com>
2017-05-13 14:09:27 +0300

    Initialization

---
+++ Fw/DB/Query/BatchIterator.php
@@ -0,0 +1,211 @@
+<?php
+/**
+ * User: Alex Gusev <alex@flancer64.com>
+ */
+
+namespace Flancer32\FixPr9621\Fw\DB\Query;
+
+use Magento\Framework\DB\Adapter\AdapterInterface;
+use Magento\Framework\DB\Select;
+
+/**
+ * This class is copy of \Magento\Framework\DB\Query\BatchIterator (version 2.1.6)
+ * with fixed 'calculateBatchSize()' method to prevent rows missing in re-index.
+ *
+ * https://github.com/magento/magento2/pull/9621
+ *
+ */
+class BatchIterator implements \Iterator
+{
+    /**
+     * @var int
+     */
+    private $batchSize;
+    /**
+     * @var AdapterInterface
+     */
+    private $connection;
+    /**
+     * @var string
+     */
+    private $correlationName;
+    /**
+     * @var Select
+     */
+    private $currentSelect;
+    /**
+     * @var bool
+     */
+    private $isValid = true;
+    /**
+     * @var int
+     */
+    private $iteration = 0;
+    /**
+     * @var int
+     */
+    private $minValue = 0;
+    /**
+     * @var string
+     */
+    private $rangeField;
+    /**
+     * @var string
+     */
+    private $rangeFieldAlias;
+    /**
+     * @var Select
+     */
+    private $select;
+
+    /**
+     * Initialize dependencies.
+     *
+     * @param Select $select
+     * @param int $batchSize
+     * @param string $correlationName
+     * @param string $rangeField
+     * @param string $rangeFieldAlias
+     */
+    public function __construct(
+        Select $select,
+        $batchSize,
+        $correlationName,
+        $rangeField,
+        $rangeFieldAlias
+    ) {
+        $this->batchSize = $batchSize;
+        $this->select = $select;
+        $this->correlationName = $correlationName;
+        $this->rangeField = $rangeField;
+        $this->rangeFieldAlias = $rangeFieldAlias;
+        $this->connection = $select->getConnection();
+    }
+
+    /**
+     * Calculate batch size for select.
+     *
+     * @param Select $select
+     * @return int
+     */
+    private function calculateBatchSize(Select $select)
+    {
+        $wrapperSelect = $this->connection->select();
+        $wrapperSelect->from(
+            $select,
+            [
+                new \Zend_Db_Expr('MAX(' . $this->rangeFieldAlias . ') as max'),
+                new \Zend_Db_Expr('COUNT(*) as cnt')
+            ]
+        );
+        $row = $this->connection->fetchRow($wrapperSelect);
+
+        /* we need collect all records with ID=$upTo over LIMIT ($this->batchSize) */
+        $upTo = $row['max'];
+        $selectTailed = $this->initSelectObject();
+        /* reset LIMIT and set upper bound for selection */
+        $selectTailed->limit(null);
+        $selectTailed->where(
+            $this->connection->quoteIdentifier($this->correlationName)
+            . '.' . $this->connection->quoteIdentifier($this->rangeField)
+            . ' <= ?',
+            $upTo
+        );
+        /* get total rows limited by "<=$upTo" only */
+        $wrapperTailed = $this->connection->select();
+        $wrapperTailed->from(
+            $selectTailed,
+            [new \Zend_Db_Expr('COUNT(*) as cnt')]
+        );
+        $rowTailed = $this->connection->fetchRow($wrapperTailed);
+
+        /* $rowTailed[cnt] should be greater or equal to $row[cnt] */
+        $totalFound = $rowTailed['cnt'];
+        $select->limit($totalFound);
+
+        $this->minValue = $upTo;
+        return intval($totalFound);
+    }
+
+    /**
+     * @return Select
+     */
+    public function current()
+    {
+        if (null == $this->currentSelect) {
+            $this->currentSelect = $this->initSelectObject();
+            $itemsCount = $this->calculateBatchSize($this->currentSelect);
+            $this->isValid = $itemsCount > 0;
+        }
+        return $this->currentSelect;
+    }
+
+    /**
+     * Initialize select object.
+     *
+     * @return \Magento\Framework\DB\Select
+     */
+    private function initSelectObject()
+    {
+        $object = clone $this->select;
+        $object->where(
+            $this->connection->quoteIdentifier($this->correlationName)
+            . '.' . $this->connection->quoteIdentifier($this->rangeField)
+            . ' > ?',
+            $this->minValue
+        );
+        $object->limit($this->batchSize);
+        /**
+         * Reset sort order section from origin select object
+         */
+        $object->order($this->correlationName . '.' . $this->rangeField . ' ' . \Magento\Framework\DB\Select::SQL_ASC);
+        return $object;
+    }
+
+    /**
+     * @return int
+     */
+    public function key()
+    {
+        return $this->iteration;
+    }
+
+    /**
+     * @return Select
+     */
+    public function next()
+    {
+        if (null == $this->currentSelect) {
+            $this->current();
+        }
+        $select = $this->initSelectObject();
+        $itemsCountInSelect = $this->calculateBatchSize($select);
+        $this->isValid = $itemsCountInSelect > 0;
+        if ($this->isValid) {
+            $this->iteration++;
+            $this->currentSelect = $select;
+        } else {
+            $this->currentSelect = null;
+        }
+        return $this->currentSelect;
+    }
+
+    /**
+     * @return void
+     */
+    public function rewind()
+    {
+        $this->minValue = 0;
+        $this->currentSelect = null;
+        $this->iteration = 0;
+        $this->isValid = true;
+    }
+
+    /**
+     * @return bool
+     */
+    public function valid()
+    {
+        return $this->isValid;
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # mage2_fix_pr9621
-Standalone fix for PR-9621
+
+Standalone fix for [PR-9621](https://github.com/magento/magento2/pull/9621) to validate proposed solution.
+
+Related issues: [#7968](https://github.com/magento/magento2/issues/7968), [#8018](https://github.com/magento/magento2/issues/8018).
+
+
+## Install
+
+
+```bash
+$ cd ${DIR_MAGE_ROOT}
+$ composer require flancer32/mage2_fix_pr9621
+$ bin/magento module:enable Flancer32_FixPr9621
+$ bin/magento setup:upgrade
+$ bin/magento cache:clean
+```
+
+## Uninstall
+
+You need an authentication keys for `https://repo.magento.com/` to uninstall any Magento 2 module. Go to your [Magento Connect](https://www.magentocommerce.com/magento-connect/customer/account/) account, section (My Account / Connect / Developer / Secure Keys) and generate pair of keys to connect to Magento 2 repository. Then place composer authentication file `auth.json` besides your `composer.json` as described [here](https://getcomposer.org/doc/articles/http-basic-authentication.md) and put your authentication keys for `https://repo.magento.com/` into the authentication file:
+```json
+{
+  "http-basic": {
+    "repo.magento.com": {
+      "username": "...",
+      "password": "..."
+    }
+  }
+}
+```
+
+Then run these commands to completely uninstall `Flancer32_FixPr9621` module: 
+```bash
+$ cd ${DIR_MAGE_ROOT}   
+$ bin/magento module:uninstall Flancer32_FixPr9621
+$ bin/magento setup:upgrade
+$ bin/magento setup:di:compile
+```
+
+Be patient, uninstall process (`bin/magento module:uninstall ...`) takes about 2-4 minutes. Remove `auth.json` file at the end:
+
+ ```bash
+$ rm ./auth.json
+```
