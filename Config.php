ef815caab3a79f2e05a72b34a1b24f210f8379bd
Alex Gusev <alex@flancer64.com>
2017-05-13 14:18:19 +0300

    Initialization

---
+++ Config.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * User: Alex Gusev <alex@flancer64.com>
+ */
+
+namespace Flancer32\FixPr9621;
+
+class Config
+{
+    const MODULE = 'Flancer32_FixPr9621';
+}
---
+++ registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * User: Alex Gusev <alex@flancer64.com>
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    \Flancer32\FixPr9621\Config::MODULE,
+    __DIR__
+);
